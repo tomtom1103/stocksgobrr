2ff2814672b3e9f47abf281aa15aa380b195a883
tomtom1103 <jonghyunlee1103@gmail.com>
2022-01-24 16:54:15 +0900

    epochtodatetimefunc

--- sentiment.py
+++ sentiment.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import pickle
 from utils import indexes
+from cleantext import clean
+import datetime
 
 tickers = pd.read_pickle('utils/tickers.pkl')
 testdata = pd.read_pickle('reports/Jan-24-2022-goes-brr.pkl')
@@ -14,3 +16,14 @@ def summary():
         df[f'{t}'] = summary
     return df
 
+def cleaner():
+    cleantitle = [clean(text, lower=False) for text in testdata['title']]
+    testdata['cleantitle'] = cleantitle
+
+    cleanselftext = [clean(text, lower=False) for text in testdata['selftext']]
+    testdata['cleanselftext'] = cleanselftext
+
+    realtime = [datetime.datetime.fromtimestamp(time) for time in testdata['created_utc']]
+    testdata['realtime'] = realtime
+
+    del testdata['title'], testdata['selftext']
