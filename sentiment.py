b51d7f585d566cb577ded19a72165616cb512475
tomtom1103 <jonghyunlee1103@gmail.com>
2022-01-24 19:09:23 +0900

    flairparsing

--- sentiment.py
+++ sentiment.py
@@ -1,29 +1,50 @@
 import pandas as pd
 import numpy as np
-import pickle
 from utils import indexes
 from cleantext import clean
 import datetime
+import flair
 
+sentiment_model = flair.models.TextClassifier.load('en-sentiment')
 tickers = pd.read_pickle('utils/tickers.pkl')
 testdata = pd.read_pickle('reports/Jan-24-2022-goes-brr.pkl')
 
 def summary():
     text = ['title', 'selftext']
-    df = pd.DataFrame(columns=['title', 'selftext'], index=indexes)
+    stats = pd.DataFrame(columns=['title', 'selftext'], index=indexes)
     for t in text:
         summary = testdata[f'{t}'].apply(len).describe()
-        df[f'{t}'] = summary
-    return df
+        stats[f'{t}'] = summary
+    return stats
 
 def cleaner():
     cleantitle = [clean(text, lower=False) for text in testdata['title']]
-    testdata['cleantitle'] = cleantitle
+    testdata['title'] = cleantitle
 
     cleanselftext = [clean(text, lower=False) for text in testdata['selftext']]
-    testdata['cleanselftext'] = cleanselftext
+    testdata['selftext'] = cleanselftext
 
     realtime = [datetime.datetime.fromtimestamp(time) for time in testdata['created_utc']]
     testdata['realtime'] = realtime
 
-    del testdata['title'], testdata['selftext']
+    score = [int(score) for score in testdata['score']]
+    testdata['score'] = score
+    return testdata
+
+def tickercount():
+    tickerlist = list(tickers['tickers'])
+    for ticker in tickerlist:
+        t_count = testdata['selftext'].str.contains(f'{ticker}').sum()
+        if t_count > 50:
+            print(f"There are {t_count} mentions of {ticker} in the posts")
+
+def flair(): #1000개의 본문파싱하는데 대략 8분걸렸다
+    probability = []
+    sentiment = []
+    for text in testdata['selftext'].to_list():
+        sentence = flair.data.Sentence(text)
+        sentiment_model.predict(sentence)
+        probability.append(sentence.labels[0].score)
+        sentiment.append(sentence.labels[0].value)
+    testdata['probability'] = probability
+    testdata['sentiment'] = sentiment
