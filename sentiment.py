0fe0a6f2ffcbd8c74257cdf6b30ce63645511f80
tomtom1103 <jonghyunlee1103@gmail.com>
2022-01-26 03:03:43 +0900

    tickerfunccomplete

--- sentiment.py
+++ sentiment.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-from utils import indexes
 from cleantext import clean
 import datetime
 import flair
@@ -9,19 +8,22 @@ from tqdm import tqdm
 import time
 
 sentiment_model = flair.models.TextClassifier.load('en-sentiment')
-tickers = pd.read_pickle('utils/tickers.pkl')
+tickers = pd.read_pickle('utils/tickers_big3.pkl')
 testdata = pd.read_pickle('reports/Jan-24-2022-goes-brr.pkl')
 flairtest = pd.read_pickle('utils/flair_test.pkl')
 
-def summary():
+
+
+def summary(): #파일의 통계치
     text = ['title', 'selftext']
-    stats = pd.DataFrame(columns=['title', 'selftext'], index=indexes)
+    idx = ['count', 'mean', 'std', 'min', '25%', '50%', '75%', 'max']
+    stats = pd.DataFrame(columns=['title', 'selftext'], index=idx)
     for t in text:
         st = testdata[f'{t}'].apply(len).describe()
         stats[f'{t}'] = st
     return stats
 
-def cleaner():
+def cleaner(): #파일 클리닝
     cleantitle = [clean(text, lower=False) for text in testdata['title']]
     testdata['title'] = cleantitle
 
@@ -35,44 +37,35 @@ def cleaner():
     testdata['score'] = score
     return testdata
 
-def tickercount_1():
-    starttime = timeit.default_timer()
-    tickers = pd.read_pickle('utils/tickers.pkl')
-    tickerlist = list(tickers['tickers'])
-    fullcounts=[]
-    for ticker in tqdm(tickerlist):
-        tickercount = []
-        sentences = [flair.data.Sentence(post) for post in testdata['selftext']] #한 포스트를 센텐스화
-        sentences = [sentence.tokens for sentence in sentences] #센텐스를 토큰화
-        sentence_tokens = [[str(token) for token in sentence] for sentence in sentences]  # 토큰을 str list 화
-        count = int(len([ticker for sentence in sentence_tokens for token in sentence if ticker in token]))
-        #각 포스트 안 해당 티커가 존재하면 티커의 수 카운트
-        tickercount.append(f'{ticker} was mentioned {count} times in this post.')
-        fullcounts.append(str(tickercount))
-
-    testdata['ticker_info'] = fullcounts
-    print(timeit.default_timer() - starttime)
-
 
 def tickercount():
-    starttime = timeit.default_timer()
-    tickers = pd.read_pickle('utils/tickers.pkl')
-    tickerlist = list(tickers['tickers'])
-    fullcounts=[]
     sentences = [flair.data.Sentence(post) for post in testdata['selftext']] #한 포스트를 센텐스화
     sentences = [sentence.tokens for sentence in sentences] #센텐스를 토큰화
     sentence_tokens = [[str(token) for token in sentence] for sentence in sentences]  # 토큰을 str list 화
 
-    for ticker in tqdm(tickerlist):
-        tickercount = []
-        count = int(len([ticker for sentence in sentence_tokens for token in sentence if ticker in token]))
-        #각 포스트 안 해당 티커가 존재하면 티커의 수 카운트
-        tickercount.append(f'{ticker} was mentioned {count} times in this post.')
-        for i in range(len(testdata)):
-            fullcounts.append(str(tickercount))
+    tickers = pd.read_pickle('utils/tickers_big3.pkl')
+    tickerlist = list(tickers['tickers'])
 
-    testdata['ticker_info'] = fullcounts
-    print(timeit.default_timer() - starttime)
+    fullcount=[]
+    for sentence in tqdm(sentence_tokens):
+        sentence = str(sentence)
+        sentence = clean(sentence, lower=False)
+        tickercount = []
+
+        for ticker in tickerlist:
+            count = 0
+            nocount = 0
+            secondcount=[]
+            if ticker in sentence:
+                count +=1
+            else:
+                nocount +=1
+            if count > 0:
+                tickercount.append(f'{ticker} was mentioned {count} times in this post.')
+
+            secondcount.append(tickercount)
+        fullcount.append(str(tickercount))
+    testdata['tickerinfo'] = fullcount
 
 
 
