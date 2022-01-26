d31e76949d05a750323e9d90d021fd652d447bed
tomtom1103 <jonghyunlee1103@gmail.com>
2022-01-26 17:17:13 +0900

    fulltest completed

--- today.py
+++ today.py
@@ -3,17 +3,12 @@ import praw
 from config import *
 from datetime import date
 import os
-import pickle
-from cleantext import clean
-import datetime
 import flair
 from tqdm import tqdm
 from sentiment import cleaner, summary, flairme, tickercount
 
 sentiment_model = flair.models.TextClassifier.load('en-sentiment')
-tickers = pd.read_pickle('utils/tickers_big3.pkl')
-testdata = pd.read_pickle('reports/Jan-24-2022-goes-brr.pkl')
-flairtest = pd.read_pickle('utils/flair_test.pkl')
+
 
 reddit = praw.Reddit(
     client_id=client_id,
@@ -38,20 +33,24 @@ def parse_submission(submission):
 
 
 def main():
-    #subreddit = reddit.subreddit('Stocks')
-    #brr = pd.DataFrame()
-    #for submission in subreddit.new(limit=None):
-    #    brr = brr.append(parse_submission(submission), ignore_index=True)
-    #brr.to_pickle(f'{reportpath}/{today}-goes-brr.pkl')
+    subreddit = reddit.subreddit('Stocks')
+    brr = pd.DataFrame()
+    print('Parsing Reddit..')
+    for submission in tqdm(subreddit.new(limit=None)):
+        brr = brr.append(parse_submission(submission), ignore_index=True)
+    brr.to_pickle(f'{reportpath}/{today}-goes-brr.pkl')
 
-    brr = pd.read_pickle('reports/Jan-24-2022-goes-brr.pkl')
+    #brr = pd.read_pickle('reports/Jan-24-2022-goes-brr.pkl')#tester
+
     summary(brr)
     cleaner(brr)
     flairme(brr)
     tickercount(brr)
     print('done!')
     print(brr)
-    brr.to_excel('fulltest.xlsx')
+    brr.to_excel(f'{reportpath}/{today}-goes-brr.xlsx')
+    brr.to_pickle(f'{reportpath}/{today}-goes-brr.pkl')
+
     return brr
 
 
