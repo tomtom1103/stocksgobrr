20c591fed60a05b536087860ca2e779e2224802f
tomtom1103 <jonghyunlee1103@gmail.com>
2022-02-03 01:41:18 +0900

    autolog class added

--- autolog.py
+++ autolog.py
@@ -1,28 +1,67 @@
-import pandas as pd
+#import pandas as pd
 import numpy as np
-from yahoo_fin import stock_info as si
-from tqdm import tqdm
-import pickle
+#from yahoo_fin import stock_info as si
+#from tqdm import tqdm
 import yfinance as yf
 
-def autolog():
-    vix = yf.Ticker("^VIX")
-    previous_close = vix.info['previousClose']
-    fifty_day_avg = vix.info['fiftyDayAverage']
-    twohundred_day_avg = vix.info['twoHundredDayAverage']
-    year_high = vix.info['fiftyTwoWeekHigh']
-    year_low = vix.info['fiftyTwoWeekLow']
+class Autolog:
 
-    vix_hist = vix.history(period='1mo')
-    vix_close = vix_hist['Close'][-1]
-    vix_lastweek_close = vix_hist['Close'][-7]
-    vix_weekly_change = (vix_close - vix_lastweek_close)/vix_lastweek_close * 100
+    def __init__(self):
+        pass
 
-    print(
-    f'''
-    Welcome back.
-    Previous close for VIX was {vix_close},
-    and its weekly change is {np.round(vix_weekly_change ,3)}%.
-        
-    '''
-    )
+
+    def daily(self, ticker):
+        company = yf.Ticker(f"{ticker}")
+        fifty_day_avg = company.info['fiftyDayAverage']
+        twohundred_day_avg = company.info['twoHundredDayAverage']
+        year_high = company.info['fiftyTwoWeekHigh']
+        year_low = company.info['fiftyTwoWeekLow']
+
+        company_hist = company.history(period='1mo')
+        company_close = company_hist['Close'][-1]
+        company_lastweek_close = company_hist['Close'][-7]
+        company_weekly_change = (company_close - company_lastweek_close)/company_lastweek_close * 100
+
+
+        print(
+            f'''
+        Welcome back.
+        Previous close for {ticker} was {np.round(company_close, 3)},
+        and its weekly change is {np.round(company_weekly_change, 3)}%.
+        50-day average: {fifty_day_avg}
+        200-day average: {twohundred_day_avg}. 
+        '''
+        )
+
+    def stockinfo(self, ticker):
+        company = yf.Ticker(f"{ticker}")
+        fifty_day_avg = company.info['fiftyDayAverage']
+        twohundred_day_avg = company.info['twoHundredDayAverage']
+        year_high = company.info['fiftyTwoWeekHigh']
+        year_low = company.info['fiftyTwoWeekLow']
+
+        company_hist = company.history(period='1mo')
+        company_close = company_hist['Close'][-1]
+        company_lastweek_close = company_hist['Close'][-7]
+        company_weekly_change = (company_close - company_lastweek_close)/company_lastweek_close * 100
+
+
+        print(
+            f'''
+        Previous close for {ticker} was ${np.round(company_close, 3)},
+        and its weekly change is {np.round(company_weekly_change, 3)}%.
+        50-day average: ${fifty_day_avg}
+        200-day average: ${twohundred_day_avg}. 
+        '''
+        )
+
+
+
+if __name__ == '__main__':
+    print("running autolog...")
+
+    today = Autolog()
+    today.daily("^VIX")
+    stock = Autolog()
+    ticker = input("Input ticker>> ")
+    stock.stockinfo(ticker)
