eea7c44893a555a4cfea315858ad40a453aaed7c
tomtom1103 <jonghyunlee1103@gmail.com>
2022-02-13 14:24:25 +0900

    added 3daychange

--- autolog.py
+++ autolog.py
@@ -18,32 +18,46 @@ class Autolog:
         vix_hist = vix.history(period='1mo')
         vix_close = vix_hist['Close'][-1]
         vix_lastweek_close = vix_hist['Close'][-7]
+        vix_threeday_close = vix_hist['Close'][-3]
         vix_weekly_change = (vix_close - vix_lastweek_close)/vix_lastweek_close * 100
+        vix_threeday_change = (vix_close - vix_threeday_close)/vix_threeday_close * 100
 
         dow = yf.Ticker(f"^DJI")
         dow_hist = dow.history(period='1mo')
         dow_close = dow_hist['Close'][-1]
         dow_lastweek_close = dow_hist['Close'][-7]
+        dow_threeday_close = dow_hist['Close'][-3]
         dow_weekly_change = (dow_close - dow_lastweek_close)/dow_lastweek_close * 100
+        dow_threeday_change = (dow_close - dow_threeday_close)/dow_threeday_close * 100
 
         snp = yf.Ticker('^GSPC')
         snp_hist = snp.history(period='1mo')
         snp_close = snp_hist['Close'][-1]
         snp_lastweek_close = snp_hist['Close'][-7]
+        snp_threeday_close = snp_hist['Close'][-3]
         snp_weekly_change = (snp_close - snp_lastweek_close)/snp_lastweek_close * 100
+        snp_threeday_change = (snp_close - snp_threeday_close)/snp_threeday_close * 100
 
         nasdaq = yf.Ticker('^IXIC')
         nasdaq_hist = nasdaq.history(period='1mo')
         nasdaq_close = nasdaq_hist['Close'][-1]
         nasdaq_lastweek_close = nasdaq_hist['Close'][-7]
+        nasdaq_threeday_close = nasdaq_hist['Close'][-3]
         nasdaq_weekly_change = (nasdaq_close - nasdaq_lastweek_close)/nasdaq_lastweek_close * 100
+        nasdaq_threeday_change = (nasdaq_close - nasdaq_threeday_close)/nasdaq_threeday_close * 100
 
         print(
             f'''
         Welcome back Tom.
         Previous close for VIX was {np.round(vix_close, 3)},
-        and its weekly change is {np.round(vix_weekly_change, 3)} %.
+        VIX weekly change is {np.round(vix_weekly_change, 3)} %.
+        VIX three day change is {np.round(vix_threeday_change, 3)} %.
         50-day average: {vix_fifty_day_avg}, 200-day average: {vix_twohundred_day_avg}.
+        
+        Three day change recap
+        Dow Jones: {np.round(dow_threeday_change, 3)} %
+        S&P 500: {np.round(snp_threeday_change, 3)} %
+        NASDAQ: {np.round(nasdaq_threeday_change, 3)} %
         
         Weekly Change recap
         Dow Jones: {np.round(dow_weekly_change, 3)} %
