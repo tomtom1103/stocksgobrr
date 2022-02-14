de8ee676fb0229113b6c87e9b911f5b7048668bc
tomtom1103 <jonghyunlee1103@gmail.com>
2022-02-14 22:21:17 +0900

    added subflowcontrol, submenus

--- autolog.py
+++ autolog.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 #from yahoo_fin import stock_info as si
-#from tqdm import tqdm
+from tqdm import tqdm
 import yfinance as yf
 import fear_and_greed
 
@@ -78,9 +78,9 @@ class Autolog:
         '''
         )
 
-    def stockinfo(self, ticker):
+    def particularstock(self, ticker):
         company = yf.Ticker(f"{ticker}")
-        company_name = company.info['shortName']
+        company_name = company.info['longName']
         fifty_day_avg = company.info['fiftyDayAverage']
         twohundred_day_avg = company.info['twoHundredDayAverage']
         year_high = company.info['fiftyTwoWeekHigh']
@@ -108,55 +108,115 @@ class Autolog:
         '''
         )
 
-    def mystocks(self):
+    def mystocks_stocklist(self):
         mystocks = pd.read_excel('mystocks.xlsx')
         mystocklist = mystocks['My Stocks'].tolist()
+        print(mystocklist)
+
+    def mystocks_twohundred(self):
+        mystocks = pd.read_excel('mystocks.xlsx')
+        mystocklist = mystocks['My Stocks']
+        count = 0
+        print(
+            f'''
+        ------------------------------
+        You currently own {mystocklist.size} stocks. Assessing 200 day average..
+        ------------------------------
+            ''')
 
         for ticker in mystocklist:
-            thomas.stockinfo(ticker)
-            '''
             company = yf.Ticker(f"{ticker}")
-            company_name = company.info['shortName']
-            fifty_day_avg = company.info['fiftyDayAverage']
+            company_name = company.info['longName']
             twohundred_day_avg = company.info['twoHundredDayAverage']
-            year_high = company.info['fiftyTwoWeekHigh']
-            year_low = company.info['fiftyTwoWeekLow']
-
             company_hist = company.history(period='1mo')
-            company_close = company_hist['Close'][-1]
-            company_lastweek_close = company_hist['Close'][-7]
-            company_threeday_close = company_hist['Close'][-3]
-            company_weekly_change = (company_close - company_lastweek_close) / company_lastweek_close * 100
-            company_threeday_change = (company_close - company_threeday_close) / company_threeday_close * 100
+            company_close = np.round(company_hist['Close'][-1])
+
+            if company_close < twohundred_day_avg:
+                count += 1
+                print(
+                f'''
+        {company_name}'s current price is below the 200 day average.
+        
+        Purchasing price: ${float(mystocks.loc[mystocks['My Stocks'] == f'{ticker}']['PP'])}
+        200 day avg: ${twohundred_day_avg}
+        Current price: ${company_close}
+        ------------------------------
+                '''
+                )
+
+            else:
+                print(
+                f'''
+        everything looks good for {company_name}.
+        ------------------------------ 
+                ''')
+
+        print(
+            f'''
+        ------------------------------
+        {count} stocks are below the 200 day average line.
+        ------------------------------
             '''
-        #print(mystocks)
+        )
 
     def news(self,ticker):
         pass
 
-
-    def flowcontrol(self):
+################################################
+    def main_menu(self):
         choice = int(input(
         """
-        --------------------------------
-        
-        What Menu would you like to see?
-        1. Particular stock
-        2. My stocks
-        3. News
-        
-        --------------------------------
+        ------------Main Menu------------
+                                         |   
+        What Menu would you like to see? |   
+        1. Particular stock              |   
+        2. My stocks                     |   
+        3. News #NOTYET                  |   
+                                         |   
+        ------------Main Menu------------
         >> """
 
         ))
 
         if choice == 1:
             ticker = input("Input ticker>> ")
-            thomas.stockinfo(ticker)
-            thomas.flowcontrol()
+            thomas.particularstock(ticker)
+            thomas.main_menu()
         elif choice == 2:
-            thomas.mystocks()
-            thomas.flowcontrol()
+            thomas.mystocks_menu()
+            thomas.main_menu()
+        elif choice == 3:
+            print('notyet')
+            thomas.main_menu()
+
+
+    def mystocks_menu(self):
+        choice = int(input(
+        """
+        ------------My Stocks------------
+                                         |
+        What Menu would you like to see? |
+        1. My Stock List                 |
+        2. 200 Day Assessment            |
+        3. News #NOTYET                  |
+        4. Back To main menu             |
+                                         |
+        ------------My Stocks------------
+        >> """
+
+        ))
+
+        if choice == 1:
+            thomas.mystocks_stocklist()
+            thomas.mystocks_menu()
+        elif choice == 2:
+            thomas.mystocks_twohundred()
+            thomas.mystocks_menu()
+        elif choice == 3:
+            print('notyet')
+            thomas.mystocks_menu()
+        elif choice == 4:
+            thomas.main_menu()
 
 
 if __name__ == '__main__':
@@ -164,4 +224,4 @@ if __name__ == '__main__':
 
     thomas = Autolog()
     thomas.daily()
-    thomas.flowcontrol()
+    thomas.main_menu()
